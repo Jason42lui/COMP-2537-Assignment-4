79a2c9c08a0080d056ed36b2bd64cc21fd11a01b
JASON_LUI <Jason53lui@gmail.com>
2022-05-28 16:20:28 -0700

    Implemented the corner cases

--- public/game.js
+++ public/game.js
@@ -1,7 +1,7 @@
 secondCard = undefined;
 firstCard = undefined;
 cardHasBeenFlipped = false;
-let lock = false;
+lock = false;
 const array = Array(9)
   .fill()
   .map(() => Math.ceil(151 * Math.random()));
@@ -9,52 +9,52 @@ const array = Array(9)
 function loadTwelvePokemon() {
   $("main").append(`
     <div id="game_grid">
-    <div class="card">
+    <div class="card" data-framework="1">
         <img id="img1" class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[0]}.png">
         <img class="back_face" src="pokeCard.png" alt="">
     </div>
-    <div class="card">
+    <div class="card" data-framework="1">
         <img id="img2"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[0]}.png">
         <img class="back_face" src="pokeCard.png" alt="">
     </div>
-    <div class="card">
+    <div class="card" data-framework="2">
         <img id="img3"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[1]}.png">
         <img class="back_face" src="/pokeCard.png" alt="">
     </div>
-    <div class="card">
+    <div class="card" data-framework="2">
         <img id="img4"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[1]}.png">
         <img class="back_face" src="pokeCard.png" alt="">
     </div>
-    <div class="card">
+    <div class="card" data-framework="3">
         <img id="img5"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[2]}.png">
         <img class="back_face" src="pokeCard.png" alt="">
     </div>
-    <div class="card">
+    <div class="card" data-framework="3">
         <img id="img6"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[2]}.png">
         <img class="back_face" src="pokeCard.png" alt="">
     </div>
-    <div class="card">
-        <img id="img5"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[3]}.png">
+    <div class="card" data-framework="4">
+        <img id="img7"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[3]}.png">
         <img class="back_face" src="pokeCard.png" alt="">
     </div>
-    <div class="card">
-        <img id="img6"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[3]}.png">
+    <div class="card" data-framework="4">
+        <img id="img8"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[3]}.png">
         <img class="back_face" src="pokeCard.png" alt="">
     </div>
-    <div class="card">
-        <img id="img5"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[4]}.png">
+    <div class="card" data-framework="5">
+        <img id="img9"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[4]}.png">
         <img class="back_face" src="pokeCard.png" alt="">
     </div>
-    <div class="card">
-        <img id="img6"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[4]}.png">
+    <div class="card" data-framework="5">
+        <img id="img10"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[4]}.png">
         <img class="back_face" src="pokeCard.png" alt="">
     </div>
-    <div class="card">
-        <img id="img5"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[5]}.png">
+    <div class="card" data-framework="6">
+        <img id="img11"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[5]}.png">
         <img class="back_face" src="pokeCard.png" alt="">
     </div>
-    <div class="card">
-        <img id="img6"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[5]}.png">
+    <div class="card" data-framework="6">
+        <img id="img12"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[5]}.png">
         <img class="back_face" src="pokeCard.png" alt="">
     </div>
 </div>
@@ -64,27 +64,27 @@ function loadTwelvePokemon() {
 function loadSixPokemon() {
   $("main").append(`
       <div id="game_grid">
-      <div class="card">
+      <div class="card" data-framework="1">
           <img id="img1" class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[0]}.png">
           <img class="back_face" src="pokeCard.png" alt="">
       </div>
-      <div class="card">
+      <div class="card" data-framework="1">
           <img id="img2"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[0]}.png">
           <img class="back_face" src="pokeCard.png" alt="">
       </div>
-      <div class="card">
+      <div class="card" data-framework="2">
           <img id="img3"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[1]}.png">
           <img class="back_face" src="/pokeCard.png" alt="">
       </div>
-      <div class="card">
+      <div class="card" data-framework="2">
           <img id="img4"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[1]}.png">
           <img class="back_face" src="pokeCard.png" alt="">
       </div>
-      <div class="card">
+      <div class="card" data-framework="3">
           <img id="img5"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[2]}.png">
           <img class="back_face" src="pokeCard.png" alt="">
       </div>
-      <div class="card">
+      <div class="card" data-framework="3">
           <img id="img6"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[2]}.png">
           <img class="back_face" src="pokeCard.png" alt="">
       </div>
@@ -92,96 +92,97 @@ function loadSixPokemon() {
                 `);
 }
 function loadSixteenPokemon() {
-    $("main").append(`
+  $("main").append(`
       <div id="game_grid">
-      <div class="card">
+      <div class="card" data-framework="1">
           <img id="img1" class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[0]}.png">
           <img class="back_face" src="pokeCard.png" alt="">
       </div>
-      <div class="card">
+      <div class="card" data-framework="1">
           <img id="img2"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[0]}.png">
           <img class="back_face" src="pokeCard.png" alt="">
       </div>
-      <div class="card">
+      <div class="card" data-framework="2">
           <img id="img3"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[1]}.png">
           <img class="back_face" src="/pokeCard.png" alt="">
       </div>
-      <div class="card">
+      <div class="card" data-framework="2">
           <img id="img4"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[1]}.png">
           <img class="back_face" src="pokeCard.png" alt="">
       </div>
-      <div class="card">
+      <div class="card" data-framework="3">
           <img id="img5"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[2]}.png">
           <img class="back_face" src="pokeCard.png" alt="">
       </div>
-      <div class="card">
+      <div class="card" data-framework="3">
           <img id="img6"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[2]}.png">
           <img class="back_face" src="pokeCard.png" alt="">
       </div>
-      <div class="card">
-          <img id="img5"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[3]}.png">
+      <div class="card" data-framework="4">
+          <img id="img7"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[3]}.png">
           <img class="back_face" src="pokeCard.png" alt="">
       </div>
-      <div class="card">
-          <img id="img6"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[3]}.png">
+      <div class="card" data-framework="4">
+          <img id="img8"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[3]}.png">
           <img class="back_face" src="pokeCard.png" alt="">
       </div>
-      <div class="card">
-          <img id="img5"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[4]}.png">
+      <div class="card" data-framework="5">
+          <img id="img9"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[4]}.png">
           <img class="back_face" src="pokeCard.png" alt="">
       </div>
-      <div class="card">
-          <img id="img6"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[4]}.png">
+      <div class="card" data-framework="5">
+          <img id="img10"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[4]}.png">
           <img class="back_face" src="pokeCard.png" alt="">
       </div>
-      <div class="card">
-          <img id="img5"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[5]}.png">
+      <div class="card" data-framework="6">
+          <img id="img11"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[5]}.png">
           <img class="back_face" src="pokeCard.png" alt="">
       </div>
-      <div class="card">
-          <img id="img6"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[5]}.png">
+      <div class="card" data-framework="6">
+          <img id="img12"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[5]}.png">
           <img class="back_face" src="pokeCard.png" alt="">
       </div>
-      <div class="card">
-        <img id="img5"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[6]}.png">
+      <div class="card" data-framework="7">
+        <img id="img13"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[6]}.png">
         <img class="back_face" src="pokeCard.png" alt="">
         </div>
-    <div class="card">
-      <img id="img6"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[6]}.png">
+    <div class="card" data-framework="7">
+      <img id="img14"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[6]}.png">
       <img class="back_face" src="pokeCard.png" alt="">
   </div>
-  <div class="card">
-      <img id="img5"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[7]}.png">
+  <div class="card" data-framework="8">
+      <img id="img15"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[7]}.png">
       <img class="back_face" src="pokeCard.png" alt="">
   </div>
-  <div class="card">
-      <img id="img6"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[7]}.png">
+  <div class="card" data-framework="8">
+      <img id="img16"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[7]}.png">
       <img class="back_face" src="pokeCard.png" alt="">
   </div>
-  <div class="card">
-  <img id="img5"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[8]}.png">
+  <div class="card" data-framework="9">
+  <img id="img17"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[8]}.png">
   <img class="back_face" src="pokeCard.png" alt="">
 </div>
-<div class="card">
-  <img id="img6"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[8]}.png">
+<div class="card" data-framework="9">
+  <img id="img18"  class="front_face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${array[8]}.png">
   <img class="back_face" src="pokeCard.png" alt="">
 </div>
   </div>
                 `);
-  }
+}
 
 function setup() {
-  console.log(array);
   loadSixteenPokemon();
   $(".card").on("click", function () {
-      if(lock) return;
+    if (lock) return;
     $(this).toggleClass("flip");
 
     if (!cardHasBeenFlipped) {
       //captured first card
       firstCard = $(this).find(".front_face")[0];
       cardHasBeenFlipped = true;
+      $(this).css("pointer-events", "none");
     } else {
+      $(this).css("pointer-events", "auto");
       secondCard = $(this).find(".front_face")[0];
       console.log(firstCard, secondCard);
       cardHasBeenFlipped = false;
@@ -195,18 +196,14 @@ function setup() {
         $(`#${secondCard.id}`).parent().off("click");
       } else {
         console.log("not a Match!");
-        lock = true
+        lock = true;
         setTimeout(() => {
           $(`#${firstCard.id}`).parent().removeClass("flip");
           $(`#${secondCard.id}`).parent().removeClass("flip");
-          lock = false
+          lock = false;
         }, 1000);
       }
     }
-
-    // reset
-    // firstCard = undefined
-    // secondCard = undefined
   });
 }
 
